9bad340d61217e1ef86bf183b6e3be418d1b7f0f
jazzy <mingchenliang954@gmail.com>
2024-07-18 18:12:52 +0800

    modify ur

--- src/component/ShareButton.tsx
+++ src/component/ShareButton.tsx
@@ -8,12 +8,19 @@ const ShareToChatButton: React.FC = () => {
         utils.openTelegramLink("https://t.me/share/url?url={https://t.me/tbook_incentive_bot?start=50636747698965}&text={helloworld}")
     }
 
+    const handleShareLink = () => {
+      utils.shareURL("https://t.me/share/url?url={https://t.me/tbook_incentive_bot?start=50636747698965}&text={helloworld}")
+
+    }
 
 
   return (
+    <>
     <button onClick={handleShareToChat}>
       Share to Chat
     </button>
+    <button onClick={handleShareLink }>share link</button>
+    </>
   );
 };
 
