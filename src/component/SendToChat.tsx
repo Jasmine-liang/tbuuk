8c326c8089bcc46e25cd46772e1d7ccbb3f0d0d5
jazzy <mingchenliang954@gmail.com>
2024-07-22 18:06:54 +0800

    add new fun

--- src/component/SendToChat.tsx
+++ src/component/SendToChat.tsx
@@ -5,19 +5,29 @@ import WebApp from '@twa-dev/sdk';
 
 const SendToChatButton: React.FC = () => {
 
-  
-  
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-               // Your browser-specific code here
-        }
-      }, []);
- 
-      const sendToChat = () => {
-        if(typeof window !== "undefined") {
-            WebApp.sendData("hhhhhhiiiii");
-        }      
+    const handleInvite = () => {
+        if (typeof window !== "undefined") 
+    {
+        const inviteMessage = "https://t.me/tbook_incentive_bot?start=50636747698965"
+        WebApp.switchInlineQuery(inviteMessage, ['users', 'groups', 'channels']);
+    
+    }
     };
+  
+        
+   
+  
+    // useEffect(() => {
+    //     if (typeof window !== "undefined") {
+    //            // Your browser-specific code here
+    //     }
+    //   }, []);
+ 
+    //   const sendToChat = () => {
+    //     if(typeof window !== "undefined") {
+    //         WebApp.sendData("hhhhhhiiiii");
+    //     }      
+    // };
 
 
     return (
@@ -29,13 +39,11 @@ const SendToChatButton: React.FC = () => {
             Show Alert
         </button>
         <p>--------------</p>
-        <button onClick={sendToChat}>Send to chat</button>
+        <button onClick={handleInvite}>Send to chat</button>
         </>
         }
-
         </div>
       );
-  
   };
   
 export default SendToChatButton;
