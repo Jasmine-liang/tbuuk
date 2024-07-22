edaf2d53ee9b61f6d123e62751949a4e061af138
jazzy <mingchenliang954@gmail.com>
2024-07-22 16:47:39 +0800

    add poe changes

--- src/component/CopyLinkButton.tsx
+++ src/component/CopyLinkButton.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initUtils } from '@telegram-apps/sdk-react';
+import { useTelegramWebApp } from './SendToChat'
 
 const CopyLinkButton: React.FC = () => {
+
+  const utils = initUtils();
+  const webApp = useTelegramWebApp();
 
   const handleCopyLink = () => {
     const link = `https://t.me/tbook_incentive_bot?start=50636747698965`;
     navigator.clipboard.writeText(link).then(() => {
       console.log('Link copied to clipboard!');
-      alert('Link copied to clipboard!');
+      
+
     }).catch(err => {
       console.error('Failed to copy link: ', err);
     });
   };
+
+
 
   return (
     <button onClick={handleCopyLink}>
