59585e8b8fe983c8c823c92c248728a61a9b49e1
jazzy <mingchenliang954@gmail.com>
2024-07-22 14:37:27 +0800

    add head

--- src/component/SendToChat.tsx
+++ src/component/SendToChat.tsx
@@ -1,42 +1,33 @@
 import React, { useEffect } from 'react';
 
 const SendToChatButton: React.FC = () => {
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-          console.log('Window object:', window);
-          console.log('Telegram object:', window.Telegram);
-          
-          if (window.Telegram && window.Telegram.WebApp) {
-            window.Telegram.WebApp.ready();
-            alert('Telegram WebApp is ready');
-          } else {
-            alert('Telegram WebApp is not available.');
-          }
-        }
-      }, []);
+  useEffect(() => {
+    const initTelegramWebApp = () => {
+      if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+        window.Telegram.WebApp.ready();
+        console.log('Telegram WebApp is ready');
+      } else {
+        console.error('Telegram WebApp is not available.');
+      }
+    };
 
-  //The sendData method in the Telegram Web Apps SDK is designed to send data to the bot that opened the web app. 
-  //The method itself does not return any value directly; instead, it sends the data to the parent chat or bot, 
-  //which can then handle the data appropriately.
-
-//Here’s a brief overview of what happens when you call sendData:
-
-//Data Transmission: The data is sent to the Telegram servers.
-//Bot Handling: The bot receives the data through an update of type web_app_data.
-
+    initTelegramWebApp();
+  }, []);
 
   const handleSendToChat = () => {
     const message = `
-    Hi friend, get your 5 scratch cards🎉💅
-    
-    Scratch to earn 🪙 Notcoin 💵20,000U 🏆TPoints
-    
-    https://t.me/tbook_incentive_bot?start=50636747698965
-  `.trim();
-    if (window.Telegram && window.Telegram.WebApp) {
+      Hi friend, get your 5 scratch cards🎉💅
+      
+      Scratch to earn 🪙 Notcoin 💵20,000U 🏆TPoints
+      
+      https://t.me/tbook_incentive_bot?start=50636747698965
+    `.trim();
+
+    if (window.Telegram?.WebApp) {
       window.Telegram.WebApp.sendData(message);
+      console.log('Data sent to chat');
     } else {
-      alert('Telegram WebApp is not available.');
+      console.error('Telegram WebApp is not available.');
     }
   };
 
