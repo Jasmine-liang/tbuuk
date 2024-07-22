ccd3c4a845fb07b38fd7a87b275a8c2cda23ac66
jazzy <mingchenliang954@gmail.com>
2024-07-22 21:52:39 +0800

    add share external

--- src/component/ExternalShare.tsx
+++ src/component/ExternalShare.tsx
@@ -13,11 +13,16 @@ const ExternalShareButton: React.FC = () => {
 
 
     const handleExternalShare = async () => {
+      const isTelegram = /Telegram/i.test(navigator.userAgent);
 
+      if (isTelegram) {
+        const url = 'https://t.me/share/url?url=https%3A%2F%2Ft.me%2Ftbook_incentive_bot%3Fstart%3D50636747698965&text=Check%20this%20out!';
+        window.location.href = url;
+      } else {
         if (typeof window !== "undefined") {
           const url = 'https://t.me/tbook_incentive_bot?start=50636747698965'; // Replace with your URL
           const text = 'Check this out!';
-      
+
           if (navigator.share) {
             navigator.share({
               title: 'Check this out!',
@@ -29,20 +34,18 @@ const ExternalShareButton: React.FC = () => {
           } else {
             const isAndroid = /Android/i.test(navigator.userAgent);
             const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-      
+
             if (isAndroid || isIOS) {
               // Use WhatsApp share link as a fallback for both platforms
-              const shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(text + ' ' + url)}`;
+              const shareUrl = `whatsapp://send?text=${encodeURIComponent(text + ' ' + url)}`;
               window.location.href = shareUrl;
             } else {
               // Fallback for unsupported devices/browsers
               alert('Your device does not support sharing.');
             }
           }
-
-
+        }
       }
-    
     }
 
 
