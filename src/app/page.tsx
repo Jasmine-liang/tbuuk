9d0b0a1f85552238a5ed57d3fd6fe7babd8c6bdf
jazzy <mingchenliang954@gmail.com>
2024-07-22 23:44:45 +0800

    add wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,20 +13,20 @@ function App() {
   const wallet = useTonWallet();
   
 
-  // if (!wallet) {
-  //   return (
-  //         // <>
-  //         //   <p>
-  //         //     To display the data related to the TON Connect, it is required to connect your wallet
-  //         //   </p>
-  //         //   <TonConnectButton />
-  //         // </>
+  if (!wallet) {
+    return (
+          // <>
+          //   <p>
+          //     To display the data related to the TON Connect, it is required to connect your wallet
+          //   </p>
+          //   <TonConnectButton />
+          // </>
       
-  //          <TonConnectButton />
-  // )
+           <TonConnectButton />
+  )
    
     
-  // }
+  }
   return (
     <>
 
@@ -50,7 +50,6 @@ function App() {
         <TonConnectButton />
       ) : ( */}
         <>
-        <TonConnectButton />
 
           <ShareToChatButton />
           <p>----------------</p>
