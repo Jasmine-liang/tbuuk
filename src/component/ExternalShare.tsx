8719c03f088404bc23ea7603a360f0a1b94ef5ca
jazzy <mingchenliang954@gmail.com>
2024-07-22 21:32:25 +0800

    add share external

--- src/component/ExternalShare.tsx
+++ src/component/ExternalShare.tsx
@@ -6,29 +6,46 @@ import WebApp from '@twa-dev/sdk';
 const ExternalShareButton: React.FC = () => {
 
     const isWebShareSupported = () => {
+      console.log("share", navigator.share )
         return navigator.share !== undefined;
       };
     
 
 
     const handleExternalShare = async () => {
+
         if (typeof window !== "undefined") {
-        if (isWebShareSupported()) {
-          try {
-            await navigator.share({
+          const url = 'https://t.me/tbook_incentive_bot?start=50636747698965'; // Replace with your URL
+          const text = 'Check this out!';
+        
+          if (navigator.share) {
+            navigator.share({
               title: 'Check this out!',
-              text: 'This is something you might like.',
-              url: 'https://example.com', // Replace with your URL
-            });
-            alert('Share successful');
-          } catch (error) {
-            console.error('Error sharing:', error);
-            alert("Error sharing:")
+              text: text,
+              url: url,
+            })
+            .then(() => console.log('Share successful'))
+            .catch((error) => console.error('Error sharing:', error));
+          } else {
+            const isAndroid = /Android/i.test(navigator.userAgent);
+            const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+        
+            if (isAndroid) {
+              const shareUrl = `intent://send/#Intent;scheme=http;package=com.android.chrome;S.browser_fallback_url=${encodeURIComponent(url)};end`;
+              window.location.href = shareUrl;
+            } else if (isIOS) {
+              const shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(text + ' ' + url)}`;
+              window.location.href = shareUrl;
+            } else {
+              // Fallback for unsupported devices/browsers
+              window.location.href = url;
+            }
           }
-        } else {
-          console.error('Web Share API not supported in this browser.');
-        }
-      }}
+
+
+      }
+    
+    }
 
 
     return (
