8acf09f1da9128e2c343e1bbdb6914e39615265f
jazzy <mingchenliang954@gmail.com>
2024-07-23 19:50:29 +0800

    add netlify config

--- src/components/CopyLinkButton/index.tsx
+++ src/components/CopyLinkButton/index.tsx
@@ -6,6 +6,10 @@ const CopyLinkButton: React.FC = () => {
 
   const handleCopyLink = () => {
     const link = `https://t.me/tbook_incentive_bot?start=50636747698965`;
+    console.log("i'm here")
+    if (typeof navigator !== 'undefined' && navigator.clipboard) {
+      console.log("i'm innnn")
+
     navigator.clipboard.writeText(link).then(() => {
       console.log('Link copied to clipboard!');
       if (typeof window !== "undefined") {
@@ -15,13 +19,14 @@ const CopyLinkButton: React.FC = () => {
     }).catch(err => {
       console.error('Failed to copy link: ', err);
     });
+  }
   };
 
 
 
   return (
     <button  className={styles.text} onClick={handleCopyLink}>
-      Copy Link
+      Copy Link aaaaaa
     </button>
   );
 };
