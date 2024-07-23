9127bb6dfb7d53bd5dbd1fb251abf1885bd93310
jazzy <mingchenliang954@gmail.com>
2024-07-23 16:59:46 +0800

    add wallet

--- src/component/SendToChat.tsx
+++ src/component/SendToChat.tsx
@@ -2,27 +2,54 @@ import React, { useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
 
 
+interface TelegramUser {
+    id: number;
+    first_name: string;
+    last_name?: string;
+    username?: string;
+    language_code?: string;
+  }
+
 
 const SendToChatButton: React.FC = () => {
+    const [user, setUser] = useState<TelegramUser | null>(null);
+
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+        const initData = WebApp.initData;
+        if (initData) {
+          try {
+            const initDataObj = JSON.parse(initData);
+            if (initDataObj.user) {
+              setUser(initDataObj.user);
+            }
+          } catch (error) {
+            console.error('Error parsing WebApp.initData:', error);
+          }
+        }
+    }
+      }, []);
 
     const handleInvite = () => {
         if (typeof window !== "undefined") 
-    {
+    // {
 
-       const message = `
-        Hi friend, get your 5 scratch cards🎉💅
+    //    const message = `
+    //     Hi friend, get your 5 scratch cards🎉💅
         
-        Scratch to earn 🪙 Notcoin 💵20,000U 🏆TPoints
+    //     Scratch to earn 🪙 Notcoin 💵20,000U 🏆TPoints
         
-        https://t.me/tbook_incentive_bot?start=50636747698965
-      `.trim()
+    //     https://t.me/tbook_incentive_bot?start=50636747698965
+    //   `.trim()
 
 
-        const inviteMessage = "https://t.me/JazzyDali_bot/testwallet?start=50636747698965"
-        WebApp.switchInlineQuery(message, ['users', 'groups', 'channels']);
-        WebApp.close();
+    //     const inviteMessage = "https://t.me/JazzyDali_bot/testwallet?start=50636747698965"
+    //     WebApp.switchInlineQuery(message, ['users', 'groups', 'channels']);
+    //     WebApp.close();
     
-    }
+    // }
+    console.log(user)
     };
 
     return (
@@ -30,6 +57,9 @@ const SendToChatButton: React.FC = () => {
          { typeof window !== "undefined" &&
           <>
            <button onClick={handleInvite}>Share to Chat</button>
+            {
+                user && <div>user: {user.id}</div>
+            }
         </>
         }
         </div>
